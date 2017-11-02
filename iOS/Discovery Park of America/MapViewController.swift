693f5baa21e899d6cd9fec301a1b3ce535e33f32
Robert Rayburn <robertrayburn@Robert-Rayburns-MacBook-Air.local>
2017-11-02 14:37:08 -0500

    Working on testing beacon functionality.

--- iOS/Discovery Park of America/MapViewController.swift
+++ iOS/Discovery Park of America/MapViewController.swift
@@ -8,15 +8,140 @@
 
 import UIKit
 import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
+}
+
+
+// TESTING FOR DISTANCE
+/*import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager:CLLocationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    
+    func rangeBeacons() {
+        let uuid = UUID(uuidString: "e2c56bd5-dffb48d2-b060d0f5-a71096e0")!
+        let major:CLBeaconMajorValue = 1
+        let minor:CLBeaconMinorValue = 238
+        let identifier = "MiniBeacon_"
+        let region = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: identifier)
+        locationManager.startRangingBeacons(in: region)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            rangeBeacons()
+        }
+    }
+    
+/*    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        guard let foundBeacon = beacons.first?.proximity else {print("No beacons were found."); return}
+        
+        let backgroundColors:UIColor = {
+            switch foundBeacon {
+            case .immediate: return UIColor.green
+            case .near: return UIColor.orange
+            case .far: return UIColor.red
+            case .unknown: return UIColor.yellow
+            }
+        }()
+        
+        self.view.backgroundColor = backgroundColors
+        
+    }*/
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
+    
+}*/
+
+
+
+
+
+
+/*import UIKit
+import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     
     //Sets the beacon region by giving the uuid that the beacon produces and a name for the beacon.
-    let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "2B162531-FD29-4758-85B4-555A6DFF00FF")! as UUID, identifier: "Beacon_01")
+    let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "74278BDA-B644-45208F0C720EAF059935")! as UUID, identifier: "MiniBeacon_")
     
-    let colors = [63449: UIColor(red:84/255, green: 77/255, blue: 160/255, alpha:1)]
+    let colors = [238: UIColor(red:84/255, green: 77/255, blue: 160/255, alpha:1)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +163,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             self.view.backgroundColor = self.colors[closestBeacon.minor.intValue]
         }
         //print(beacons)
-    }
+    }*/
     
     /*    var mapView: MKMapView!
      var locationManager: CLLocationManager?
@@ -119,6 +244,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
      //This is a method from MKMapViewDelegate, fires up when the user`s location changes
      let zoomedInCurrentLocation = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 500, 500)
      mapView.setRegion(zoomedInCurrentLocation, animated: true)
-     }*/
-}
+     }
+}*/
 
